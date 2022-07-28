6f63c719a78cb3aebb47b00fa868f79e2df9a125
Thanhlongcs <thanhlongcf010@gmail.com>
2022-07-28 17:00:49 +0700

    up

---
+++ src/rikkei/academy/StopWatch.java
@@ -0,0 +1,30 @@
+package rikkei.academy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalTime;
+
+public class StopWatch {
+    private long   startTime, endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void stop(){
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return endTime - startTime;
+    }
+}
